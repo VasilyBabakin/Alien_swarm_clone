be6b441118168f03ba8aa5ab89760a29a6a742bb
VasilyBabakin <165548616+VasilyBabakin@users.noreply.github.com>
2024-09-23 15:16:32 +0300

    Added fork and refactor

--- src/main.py
+++ src/main.py
@@ -4,12 +4,12 @@ import sys
 
 from setting import Settings
 from ship import Ship
+
+
 class AlienInvasion:
     def __init__(self):
         pygame.init()
         self.clock = pygame.time.Clock()
-
-
         self.settings = Settings()
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption('Alien Invasion')
@@ -18,16 +18,22 @@ class AlienInvasion:
     def run_game(self):
         while True:
             self._check_events()
-            pygame.display.flip()
+            self._update_screen()
             self.clock.tick(60)
-
+    '''We are wainting for closing the game'''
     def _check_events(self):
         for event in pygame.event.get():
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+    '''We are filling screen and using blitme to display PC'''
+
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        pygame.display.flip()
+        
+
 if __name__ == '__main__':
     ai = AlienInvasion()
     ai.run_game()
